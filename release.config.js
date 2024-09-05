6fa9c72dd2464dce82de0e64cd017ba2d3db38cb
Ryan Elliott-Potter <me@ryanep.com>
2024-09-05 17:34:00 +0100

    chore: update release config

---
+++ release.config.js
@@ -0,0 +1,11 @@
+const releaseConfig = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/npm",
+  ],
+};
+
+export default releaseConfig;
